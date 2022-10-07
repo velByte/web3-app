055ba15e6835226e28a965d5e898eeabbe1d3df8
velByte <67099502+velByte@users.noreply.github.com>
2022-10-07 13:30:07 +0200

    Added Client Language Support

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+import { Loader } from "./";
+import welcomeTexts from "../sprach_verwaltung/welcomeTexts.json";
+import languages from "../sprach_verwaltung/supported";
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  const userLanguage =
+    window.navigator.userLanguage || window.navigator.language;
+  const supportedLanguages = languages();
+  const [language, setLanguage] = useState(
+    supportedLanguages.includes(userLanguage) ? userLanguage : "en"
+  );
+  return (
+    // This is centering the entire content in the middle of the screen
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            {welcomeTexts.main_title[language]}
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            {welcomeTexts.main_subtitle[language]}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
---
+++ client/src/sprach_verwaltung/supported.jsx
@@ -0,0 +1,5 @@
+const languages = () => {
+  return ["en", "de", "bg"];
+};
+
+export default languages;
